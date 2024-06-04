7b8fd70015f3bc0133b47de88b441ed57989f685
arijitdas122001 <aribwnlvpgbwn@gmail.com>
2024-06-05 00:40:32 +0530

    delete comment controller

--- src/app/api/(comment)/Delete-Comment/route.ts
+++ src/app/api/(comment)/Delete-Comment/route.ts
@@ -1,12 +1,16 @@
 import CommentModel from "@/Model/Comment";
 import ApiResponse from "@/utils/ApiResponse";
 
-export async function GET(req:Request) {
+export async function DELETE(req:Request) {
     try {
-        const {username}=await req.json();
+        const {username,del_user_id}=await req.json();
         const model=await CommentModel.findOne({to_username:username});
-        return ApiResponse(200,"Got it",true);
+        await CommentModel.findByIdAndUpdate(
+            model?._id,
+            {$pull:{Comments:{user_id:del_user_id}}},
+        )
+        return ApiResponse(200,"Deleted It",true);
     } catch (error) {
-        return ApiResponse(400,"Error",false);
+        return ApiResponse(400,"Error while deleting the comment",false);
     }
 }
