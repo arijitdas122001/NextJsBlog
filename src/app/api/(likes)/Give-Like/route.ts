7f5bd088d36cb0574174f36e4eede704bc34b08d
arijitdas122001 <aribwnlvpgbwn@gmail.com>
2024-06-30 01:14:14 +0530

    Give like route api updated

--- src/app/api/(likes)/Give-Like/route.ts
+++ src/app/api/(likes)/Give-Like/route.ts
@@ -10,25 +10,25 @@ export async function POST(
     try {
         //collect blog_id from parameter and to_username from data so pass on body
         //from_username will come from session pass on body
-        const {recv_like_username,giving_like_id,blog_id}=await req.json();
+        const {recv_like_username,from_username,blog_id}=await req.json();
         //find the model and with the blog username
         const model=await LikeModel.findOne({to_username:recv_like_username});
         // if there is no modal then create one
         if(!model){
             const newModel=new LikeModel({
                 to_username:recv_like_username,
-                from_username:[giving_like_id]
+                from_username:[from_username]
             });
             await newModel.save();
         }else{
         //push the session_user to the from_user
-        model.from_username.push(giving_like_id);
+        model.from_username.push(from_username);
         await model?.save();
         }
         //find the corresponding blog
         const cors_blog=await BlogModel.findById(blog_id);
         // push the giving like user to the like count
-        cors_blog?.likecnt.push(giving_like_id);
+        cors_blog?.likecnt.push(from_username);
         await cors_blog?.save();
         return ApiResponse(200,"Given Like",true);
     } catch (error) {
