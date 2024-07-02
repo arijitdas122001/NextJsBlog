803da1fdccf56b5cc2b05651e134f156a8ddfdef
arijitdas122001 <aribwnlvpgbwn@gmail.com>
2024-07-03 01:05:43 +0530

    Hero Sectio and navbar designed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import AuthProvider from "@/context/AuthProvider";
-
+import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
+        <Navbar/>
+        {children}
         <Toaster/>
-      </body>
+      </body>  
       </AuthProvider>
     </html>
   );
