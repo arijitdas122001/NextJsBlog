f6aa8bdfaf5c92433a0424fea9e257c470dbe376
arijitdas122001 <aribwnlvpgbwn@gmail.com>
2024-07-05 23:40:29 +0530

    dashboard ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,25 +2,37 @@ import React from 'react'
 
 const DashBoard = () => {
   return (
-<div className="flex *:items-center min-h-screen">
-    <div className="w-full mx-w-md p-8 relative">
-<div className="bg-slate-400 fixed left-2/4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis optio fuga quaerat eaque quibusdam autem impedit. Maiores libero beatae fugit itaque nemo placeat illo labore, minus, quas, illum nobis nostrum.</div>
-  <div className="flex-1 w-2/3 bg-pink-200">
-    <div className="p-5">
-      <div className="flex gap-2">
+<div className="flex justify-center min-h-screen">
+<div className="w-full max-w-5xl space-y-8 bg-purple-800 flex gap-8">
+  <div className="flex-2 p-2">
+  <div className="bg-pink-200">
+    <div className="p-7">
+      <div className="flex gap-8 w-full">
         <div className="flex-1 flex flex-col gap-3">
           <div>User1</div>
           <div>You don’t actually NEED if statements (ever)</div>
           <div>Frontend dev will never be the same with the new tag…</div>
         </div>
-        <div className="flex-2 bg-black">Image</div>
+        <div className="bg-black">Image</div>
       </div>
       <div>LIke</div>
     </div>
   </div>
   </div>
-</div>
-
+  <div className="flex-1">
+  <div className="bg-slate-400">
+    <div className='topicks'>
+      <div></div>
+      <div></div>
+    </div>
+    <div className='tags'>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
   )
 }
 
