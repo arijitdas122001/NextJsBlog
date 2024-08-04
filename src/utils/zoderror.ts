53412964bdd4313ddcba3298511f68ddb90f2740
arijitdas122001 <aribwnlvpgbwn@gmail.com>
2024-08-05 01:10:39 +0530

    deployment changes #7

--- src/utils/zoderror.ts
+++ src/utils/zoderror.ts
@@ -1,3 +1,5 @@
-class ZodError extends Error {
-    issues: ZodIssue[];
-  }
+// import * as z from "zod";
+// const myError = new z.ZodError([]);
+// class ZodError extends Error{
+//   issues:z.ZodIssue[]
+// }
